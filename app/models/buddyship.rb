0048b9cd050c3c31b7c7f662a1df14faa3bfc599
vaidehijoshi <vaidehi.sj@gmail.com>
2014-12-02 14:54:49 -0500

    adds inversify and destroy_relationship methods to buddyship model

--- app/models/buddyship.rb
+++ app/models/buddyship.rb
@@ -2,4 +2,17 @@ class Buddyship < ActiveRecord::Base
   belongs_to :student
   belongs_to :buddy, class_name: 'Student', foreign_key: 'buddy_id'
   belongs_to :course_section
+  after_create :inversify
+  validates_uniqueness_of :student, :scope => :buddy
+  validates_uniqueness_of :buddy, :scope => :student
+
+  def inversify
+    self.buddy.buddyships.create(:course_section_id => self.course_section_id, :buddy_id => self.student.id)
+  end
+
+  def destroy_relationship
+    self.class.where(:student_id => self.buddy_id, :buddy_id => self.student_id, :course_section_id => self.course_section_id).first.destroy
+    self.destroy
+  end
+
 end
