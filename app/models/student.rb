ca09e27d5da2df6a7b5da2f4eb94a5b2b78bebea
Rose Weixel <rose.weixel@gmail.com>
2014-12-05 14:02:58 -0500

    make some methods connecting students to their scores

--- app/models/student.rb
+++ app/models/student.rb
@@ -20,5 +20,35 @@ class Student < ActiveRecord::Base
     guardians.pluck(:id)
   end
 
+  def is_overdue?(assignment)
+    if !assignment.students.include?(self) || !assignment.date_due || assignment.date_due > Date.today || assignment.date_due < Date.today && score_for_assignment(assignment) != nil
+    end
+    true
+  end
+
+  def score_for_assignment(assignment)
+    if !Score.where(assignment_id: assignment.id, student_id: self.id).empty?
+      Score.where(assignment_id: assignment.id, student_id: self.id).first.points_earned
+    end
+  end
+
+  def percent_score_for_assignment(assignment)
+    if score_for_assignment(assignment) && assignment.points
+      return (score_for_assignment(assignment).to_f / assignment.points) * 100
+    end
+  end
+
+  def all_assignments_info
+    assignment_score_hash = {}
+    self.assignments.each do |assignment|
+      assignment_score_hash[assignment.course_section.name] ||= []
+      assignment_score_hash[assignment.course_section.name] << {name: assignment.name, teacher: assignment.teacher.full_name, date_assigned: assignment.date_assigned, date_due: assignment.date_due, points_score: score_for_assignment(assignment), overdue: is_overdue?(assignment), percent_score: percent_score_for_assignment(assignment), max_points: assignment.points || "no max points set"}
+    end
+    assignment_score_hash
+  end
+
+  def overall_average
+  end
+
 
 end
