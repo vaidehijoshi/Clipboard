fe44bb3e4e0c69fc1b167df678b22f2475c4a336
molgin <molgin@users.noreply.github.com>
2014-12-07 16:23:28 -0500

    bug fixes & add colors to scores

--- app/models/score.rb
+++ app/models/score.rb
@@ -7,6 +7,26 @@ class Score < ActiveRecord::Base
   validates :points_earned, presence: true
   validate :points_not_more_than_possible
 
+  def color_class
+    if points_earned
+      ratio = points_earned / assignment.points.to_f
+      case
+      when ratio >= 0.9
+        "info"
+      when ratio >= 0.8
+        "success"
+      when ratio >= 0.7
+        "warning"
+      when ratio < 0.7
+        "danger"
+      # when ratio < 0.6
+      #   "active"
+      end
+    end
+  end
+
+
+  private
   def points_not_more_than_possible
     if points_earned && assignment.points
       if points_earned > assignment.points
